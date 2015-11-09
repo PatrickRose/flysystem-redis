706211bdcae1cc5b006d8ac454bcdbed4952c553
Patrick Rose <pjr0911025@googlemail.com>
2015-11-08 19:07:03 -0500

    Applied fixes from StyleCI

--- tests/spec/RedisAdapterSpec.php
+++ tests/spec/RedisAdapterSpec.php
@@ -2,22 +2,20 @@
 
 namespace spec\PatrickRose\Flysystem\Redis;
 
-use League\Flysystem\Adapter\AbstractAdapter;
 use League\Flysystem\AdapterInterface;
 use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
 
 class RedisAdapterSpec extends ObjectBehavior
 {
     /**
      * @param \Predis\ClientInterface $redis
      */
-    function let($redis)
+    public function let($redis)
     {
         $this->beConstructedWith($redis);
     }
 
-    function it_is_initializable()
+    public function it_is_initializable()
     {
         $this->shouldHaveType('PatrickRose\Flysystem\Redis\RedisAdapter');
         $this->shouldHaveType(AdapterInterface::class);
